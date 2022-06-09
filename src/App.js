b0fccabbe28588f1deba8bae5b3877828906c56f
Shee <2107154@cognizant.com>
2022-06-09 18:24:37 +0530

    handson 2 commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome the first session of React!!</h1>
+      {/* <h1>Welcome the first session of React!!</h1> */}
+      <Home />
+      <Contact />
+      <About />
     </div>
   );
 }
